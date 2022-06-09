19a2ff8d0d224fa637d0abf3b2a6087d03bd3701
edurevsky <edurevsky@gmail.com>
2022-06-09 18:04:09 -0300

    some stuff

--- src/playground/core.clj
+++ src/playground/core.clj
@@ -1,6 +1,33 @@
-(ns playground.core)
+(ns playground.core
+  (:use clojure.pprint)
+  (:import (java.time LocalDateTime)))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(println
+  (if nil "true" "false"))
+
+(defn cool-function
+  [x y]
+  (let [result (+ x y)]
+    (if (even? result)
+      result
+      (recur x (inc y)))))
+
+(println (cool-function 1 0))
+
+(println (System/currentTimeMillis))
+
+(let [current-date (LocalDateTime/now)]
+  (.getMonthValue current-date))
+
+(let [message "Hello World"]
+  (->> message
+       (.split #"\[a-zA-Z]+ ")
+       vec
+       println))
+
+(do
+  (println "begin")
+  (Thread/sleep 1000)
+  (println "end"))
+
+
